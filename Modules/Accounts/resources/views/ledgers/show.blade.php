ec7c4ff3c0003f1f2aa642f6f5b30478451c2587
Naimul Islam <naimcse56@gmail.com>
2024-11-25 21:34:36 +0600

    css

--- Modules/Accounts/resources/views/ledgers/show.blade.php
+++ Modules/Accounts/resources/views/ledgers/show.blade.php
@@ -3,7 +3,7 @@
 <div class="modal fade" id="showModal" data-bs-backdrop="static" data-bs-keyboard="false">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
-            <div class="modal-header bg-success-subtle">
+            <div class="modal-header bg-success text-white">
                 <h5 class="modal-title">{{$ledger->name}} Information</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
