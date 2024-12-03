afa553a3974ca98d05415e47d53cc844dcb0800d
MD NAIMUL ISLAM <naimcse56@gmail.com>
2024-12-03 12:16:05 +0600

    css

--- Modules/Accounts/resources/views/cash_payment_multiple/components/action.blade.php
+++ Modules/Accounts/resources/views/cash_payment_multiple/components/action.blade.php
@@ -3,7 +3,7 @@
     <button type="button" class="btn btn-sm btn-info detail_info" data-route="{{ route('multi-cash-payment.show',encrypt($row->id)) }}"><i class="fa fa-eye"></i></button>
     @if ($row->is_approve != 1)
     <a href="{{ route('multi-cash-payment.edit',encrypt($row->id)) }}" class="btn btn-sm btn-warning edit_leadger"><i class="fa fa-edit"></i></a>
-    <button type="button" onclick="deleteData('Cash Payment Voucher', '{{ route('multi-cash-payment.delete') }}', {{ $row->id }})" class="btn btn-sm btn-outline-danger" id="basicAlert">
+    <button type="button" onclick="deleteData('Cash Payment Voucher', '{{ route('multi-cash-payment.delete') }}', {{ $row->id }})" class="btn btn-sm btn-danger" id="basicAlert">
         <i class="fa fa-trash"></i>
     </button>
     @endif
