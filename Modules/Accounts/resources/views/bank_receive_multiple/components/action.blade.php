23f45556320dcdaedd6a37616f478c6c39cc5e3e
MD NAIMUL ISLAM <naimcse56@gmail.com>
2024-12-04 12:44:44 +0600

    bank and cash rcv

--- Modules/Accounts/resources/views/bank_receive_multiple/components/action.blade.php
+++ Modules/Accounts/resources/views/bank_receive_multiple/components/action.blade.php
@@ -3,7 +3,7 @@
     <button type="button" class="btn btn-sm btn-info detail_info" data-route="{{ route('multi-bank-receive.show',encrypt($row->id)) }}"><i class="fa fa-eye"></i></button>
     @if ($row->is_approve != 1)
     <a href="{{ route('multi-bank-receive.edit',encrypt($row->id)) }}" class="btn btn-sm btn-warning edit_leadger"><i class="fa fa-edit"></i></a>
-    <button type="button" onclick="deleteData('Bank Payment Voucher', '{{ route('multi-bank-receive.delete') }}', {{ $row->id }})" class="btn btn-sm btn-danger" id="basicAlert">
+    <button type="button" onclick="deleteData('Bank Receive Voucher', '{{ route('multi-bank-receive.delete') }}', {{ $row->id }})" class="btn btn-sm btn-danger" id="basicAlert">
         <i class="fa fa-trash"></i>
     </button>
     @endif
