52974860088fca8cf033789d018cc0e244b192c3
MOnirul Islam <monirjss@gmail.com>
2024-12-10 09:42:14 +0600

    color change design  optimization

--- resources/views/backend/partials/sidebar.blade.php
+++ resources/views/backend/partials/sidebar.blade.php
@@ -12,7 +12,7 @@
         </div>
         <div class="sb-sidenav-footer">
             <div class="small">Logged in as:</div>
-            <div class="small text-white">{{auth()->user()->name}}</div>
+            <div class="small text-black">{{auth()->user()->name}}</div>
         </div>
     </nav>
 </div>
