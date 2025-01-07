e63bbeb9d6528a4f8438a516017eb658522ac95f
MD NAIMUL ISLAM <naimcse56@gmail.com>
2025-01-07 14:53:48 +0600

    staff show working

--- resources/views/backend/profiles/profile.blade.php
+++ resources/views/backend/profiles/profile.blade.php
@@ -12,10 +12,9 @@ My Account
                     <form method="POST" action="{{ route('profile_update') }}" class="text-start mb-3">
                         @csrf
                         <div class="row">
-                            <x-common.server-side-select :required="true" column=6 name="post" id="post" class="post" disableOptionText="Select Sample" label="Sample"></x-common.server-side-select>
                             <x-common.input :required="true" column=6 id="name" name="name" label="Name" placeholder="Name" :value="old('name',auth()->user()->name)"></x-common.input>
                             <x-common.input :required="true" column=6 id="username" name="username" label="Username" placeholder="Username" :value="old('name',auth()->user()->username)"></x-common.input>
-                            <x-common.input :required="true" column=6 type="email" id="email" name="email" label="Email" placeholder="Email" :value="old('name',auth()->user()->email)" readonly></x-common.input>
+                            <x-common.input :required="true" column=6 type="email" id="email" name="email" label="Email" placeholder="Email" :value="old('email',auth()->user()->email)" readonly></x-common.input>
                             <x-common.input :required="true" column=6 type="password" id="password" name="password" label="Password" placeholder="Password" :value="old('password')"></x-common.input>
                             <x-common.input :required="true" column=6 type="password" id="password_confirmation" name="password_confirmation" label="Confirm Password" placeholder="Confirm Password" :value="old('password_confirmation')"></x-common.input>
                             <x-common.button column=12 type="submit" id="update_btn" class="btn-primary btn-120" :value="'Update'"></x-common.button>
@@ -27,30 +26,3 @@ My Account
     </div><!-- end row -->
 </div>
 @endsection
-
-@push('scripts')
-    <script>
-        (function($) {
-            "use strict";
-            $("#post").select2({
-                    ajax: {
-                        url: "{{route('post-select')}}",
-                        type: "get",
-                        dataType: 'json',
-                        delay: 250,
-                        data: function (params) {
-                                var query = {
-                                    search: params.term,
-                                    page: params.page || 1,
-                                }
-                                return query;
-                        },
-                        cache: false
-                    },
-                    escapeMarkup: function (m) {
-                        return m;
-                    }
-                });
-        })(jQuery);
-    </script>
-@endpush
