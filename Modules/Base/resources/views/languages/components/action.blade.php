a5e2975fe201469444bdd8cd28041e107b07212e
MD NAIMUL ISLAM <naimcse56@gmail.com>
2025-01-19 09:22:13 +0600

    header css for mobile view

--- Modules/Base/resources/views/languages/components/action.blade.php
+++ Modules/Base/resources/views/languages/components/action.blade.php
@@ -1,6 +1,6 @@
 <div class="btn-group" role="group" aria-label="Basic example">
     <button type="button" class="btn btn-sm btn-warning edit_leadger" data-id="{{ encrypt($row->id) }}"><i class="fa fa-edit"></i></button>    
-    <button type="button" class="btn btn-sm btn-info detail_info" data-route="{{ route('language.translate_view',$row->id) }}"> Translation
+    {{-- <button type="button" class="btn btn-sm btn-info detail_info" data-route="{{ route('language.translate_view',$row->id) }}"> Translation --}}
     </button>
     @if ($row->id > 3)
         <button type="button" onclick="deleteData('Language ', '{{ route('language.delete') }}', {{ $row->id }})" class="btn btn-sm btn-danger" id="basicAlert">
